a04cd8a86f6c7be01fc4a7ceb661317b50e073c9
Harsh Sinha <85688366+harsh1sinha@users.noreply.github.com>
2025-08-10 15:48:28 +0530

    event listener and output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input,Output,EventEmitter, input,computed } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,15 +9,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
+    return 'assets/users/' + this.avatar;
   }
-
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]; 
-    // this is actually changing the state of selectedUser
+    this.select.emit(this.id);
   }
 }
