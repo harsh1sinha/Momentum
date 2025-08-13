927d654056195d068300cecf245eaa7b6a3bd448
Harsh Sinha <85688366+harsh1sinha@users.noreply.github.com>
2025-08-13 23:40:53 +0530

    completed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,19 @@
 import { Component, Input,Output,EventEmitter, input,computed } from '@angular/core';
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   // avatar = input.required<string>();
